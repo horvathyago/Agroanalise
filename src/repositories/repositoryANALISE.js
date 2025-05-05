19e3231db4709721385a7713e1af02d234377c7b
horvathyago <yagohorvath1@gmail.com>
2025-05-05 18:49:10 -0300

    v2

--- src/repositories/repositoryANALISE.js
+++ src/repositories/repositoryANALISE.js
@@ -8,19 +8,19 @@ async function Listar() {
 
 async function Inserir(nome, email, senha) {
     let sql = "INSERT INTO Usuarios(nome, email, senha) VALUES (?, ?, ?)";
-    const [usuarios] = await con.query(sql, [nome, email, senha]);
+    const [usuarios] = await (con.connection.execute(sql, [nome, email, senha]));
     return usuarios;
 }
 
 async function Editar(id_usuario, nome, email, senha) {
     let sql = "UPDATE Usuarios SET nome=?, email=?, senha=? WHERE id_usuario=?";
-    const [usuarios] = await con.query(sql, [nome, email, senha, id_usuario]);
+    const [usuarios] = await (con.connection.execute(sql, [nome, email, senha, id_usuario]));
     return { id_usuario };
 }
 
 async function Excluir(id_usuario) {
     let sql = "DELETE FROM Usuarios WHERE id_usuario=?";
-    const [usuarios] = await con.query(sql, [id_usuario]);
+    const [usuarios] = await (con.connection.execute(sql, [id_usuario]));
     return { mensagem: "usuário excluído" };
 }
 
